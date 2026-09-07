89c394efa68b64ab2bbbb37a92789f11a4e01845
Nikhil Sabharwal <46387755+NSabharwal-27@users.noreply.github.com>
2026-09-07 12:24:47 -0400

    created a 32-bit full-adder

---
+++ mux2.sv
@@ -0,0 +1,21 @@
+/*
+ * @brief take in 3 32-bit signals and perform addition
+ */
+module fulladder32(   
+            input   logic [31:0] a, 
+            input   logic [31:0] b,
+            input   logic [31:0] cin,
+            output  logic [31:0] s,
+            output  logic [31:0] cout
+);
+
+logic [31:0] p;
+logic [31:0] g;
+
+assign p = a ^ b;
+assign g = a & b;
+
+assign s = p ^ cin;
+assign cout = g | (p & cin);
+
+endmodule
