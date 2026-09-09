296f3b7ff80bb9551c6cbb370fdd9450dc04752f
Nikhil Sabharwal <46387755+NSabharwal-27@users.noreply.github.com>
2026-09-09 08:46:36 -0400

    OK, so I tried compiling my project and it did not work, a few tweaks later this does compile.

--- register_file.sv
+++ register_file.sv
@@ -16,17 +16,19 @@ module register_file(
         output logic [31:0] o_rs2
 );
 
-logic [31:0] register_file;
+logic [31:0] register_file [0:31];
 
 assign o_rs1 = (i_rs1_addr == 5'b0) ? 5'b0 : register_file[i_rs1_addr];
 assign o_rs2 = (i_rs2_addr == 5'b0) ? 5'b0 : register_file[i_rs2_addr];
 
 always_ff @(posedge clk or negedge rst) begin
     if (!rst) begin
-        register_file <= '{default: '0};
+        for(int i = 0; i < 32; i = i+1) begin
+            register_file[i] <= 32'b0;
+        end
     end
 
-    if (i_we && i_rs1_addr != 5'b0) begin
+    else if (i_we && i_rd_addr != 5'b0) begin
         register_file[i_rd_addr] <= i_rd_data;
     end        
 end
