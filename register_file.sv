2ce9fe0f2c4adda379658d87eb8992a3679f4296
Nikhil Sabharwal <46387755+NSabharwal-27@users.noreply.github.com>
2026-09-09 08:35:48 -0400

    Updated and completed the initial register file, design. Still needs to be tested.

--- register_file.sv
+++ register_file.sv
@@ -1,23 +1,34 @@
 /*
- * R, I, S, SB Instructions all read
- * rs1 from bits 19-15 so we need 5 bits for input reg
+ * RV-32I has 32 General purpose 32-bit registers,
+ * register x0 is always hardwired to be 0. 
  */
 
 module register_file(
         input logic clk,
         input logic rst,
-        input logic i_w_enable,
-        input logic i_reg_write,
-        input logic [4:0] i_rs1,
-        input logic [4:0] i_rs2,
-        input logic [4:0] i_w_register,
-        input logic [31:0] i_w_data,
-        output logic [31:0] o_r_data_1,
-        output logic [31:0] o_r_data_2
+        input logic i_we,
+        input logic [31:0] i_rd_data,
+        input logic [4:0]  i_rd_addr,
+        input logic [4:0]  i_rs1_addr,
+        input logic [4:0]  i_rs2_addr,
+
+        output logic [31:0] o_rs1,
+        output logic [31:0] o_rs2
 );
 
-always_ff @(posedge clk)
-    if ()
+logic [31:0] register_file;
 
+assign o_rs1 = (i_rs1_addr == 5'b0) ? 5'b0 : register_file[i_rs1_addr];
+assign o_rs2 = (i_rs2_addr == 5'b0) ? 5'b0 : register_file[i_rs2_addr];
+
+always_ff @(posedge clk or negedge rst) begin
+    if (!rst) begin
+        register_file <= '{default: '0};
+    end
+
+    if (i_we && i_rs1_addr != 5'b0) begin
+        register_file[i_rd_addr] <= i_rd_data;
+    end        
+end
 
 endmodule
