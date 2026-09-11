d58e3826c952abe5093b6a8c7ffe1dcac82f6e08
Nikhil Sabharwal <46387755+NSabharwal-27@users.noreply.github.com>
2026-09-10 23:33:06 -0400

    comments in regfile

--- register_file.sv
+++ register_file.sv
@@ -6,12 +6,15 @@
 module register_file(
         input logic clk,
         input logic rst,
+
+        /* All Input signal we will need */
         input logic i_we,
         input logic [31:0] i_rd_data,
         input logic [4:0]  i_rd_addr,
         input logic [4:0]  i_rs1_addr,
         input logic [4:0]  i_rs2_addr,
 
+        /* All the output signal we will need */
         output logic [31:0] o_rs1,
         output logic [31:0] o_rs2
 );
@@ -22,6 +25,7 @@ assign o_rs1 = (i_rs1_addr == 5'b0) ? 5'b0 : register_file[i_rs1_addr];
 assign o_rs2 = (i_rs2_addr == 5'b0) ? 5'b0 : register_file[i_rs2_addr];
 
 always_ff @(posedge clk or negedge rst) begin
+    
     if (!rst) begin
         for(int i = 0; i < 32; i = i+1) begin
             register_file[i] <= 32'b0;
